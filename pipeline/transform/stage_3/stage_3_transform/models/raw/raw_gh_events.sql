9e522f67e55a34a71b913baf0125d88473e12f69
krishnasadhu <nitish.sadhu@icloud.com>
2026-04-09 17:16:24 +0530

    partitioned raw_gh_events

--- pipeline/transform/stage_3/stage_3_transform/models/raw/raw_gh_events.sql
+++ pipeline/transform/stage_3/stage_3_transform/models/raw/raw_gh_events.sql
@@ -1,18 +1,24 @@
-{{ config(materialized = "incremental", unique_key = "surrogate_id") }}
+{{ config(materialized = "incremental",
+            unique_key = "surrogate_id",
+            partition_by={
+                "field": "created_at",
+                "data_type": "date"
+            })
+}}
 
 SELECT * EXCEPT(rnum) FROM(
     SELECT
-        *, row_number() over(partition by surrogate_id) AS rnum
+        *
     FROM {{ source('gh_events', 'raw_gh_events_ext') }}
     WHERE id IS NOT NULL
+    {% if is_incremental() %}
+        AND year = EXTRACT(YEAR FROM CURRENT_DATE - 2)
+        AND month = EXTRACT(MONTH FROM CURRENT_DATE - 2)
+        AND day = EXTRACT(DAY FROM CURRENT_DATE - 2)
+        AND DATE(created_at) = CURRENT_DATE - 2
+    {% endif %}
 )
 WHERE rnum = 1
-{% if is_incremental() %}
-    AND CAST(created_at AS DATE) = CURRENT_DATE - 2
- ---(SELECT TIMESTAMP_SUB(MAX(created_at), INTERVAL 1 HOUR) FROM {{ this }})
-{% endif %}
-
-
 
 
 
